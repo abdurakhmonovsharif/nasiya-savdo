0522d9e52d9c0cbe5c2e9145f8e6e2738bda12a9
Abdurakhmonov Sharif <sharifabduraxmonov@mail.ru>
2025-01-18 16:49:25 +0500

    first commit

---
+++ src/main/java/study/nires/uz/nasiyasavdobackend/NasiyaSavdoBackendApplication.java
@@ -0,0 +1,13 @@
+package study.nires.uz.nasiyasavdobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NasiyaSavdoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NasiyaSavdoBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/study/nires/uz/nasiyasavdobackend/NasiyaSavdoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package study.nires.uz.nasiyasavdobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NasiyaSavdoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
